82f3408e2cd3de16c2de108610b5cadf35e29e86
ctiennot <c.tiennot@gmail.com>
2016-05-20 20:00:39 +0200

    Exploratory analysis Plot some trajectories and their log-amplitudes/log-variances by signal quality

---
+++ src/exploratory.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+This file gives some insights about the data, it is an exploratory analysis
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import functions as fun
+
+np.random.seed(seed=1789)
+
+# Load data
+X, y = fun.load_quality_dataset()
+
+pos = np.array(range(y.shape[0]))[y==1.] # good signal index
+neg = np.array(range(y.shape[0]))[y==0.] # bad signal index
+
+# Look at random trajectories in the data
+plt.figure()
+axes = plt.gca()
+axes.set_ylim([-500,500])
+for i in np.random.choice(pos, 300, False):
+    plt.plot(range(500), X[i,500:1000], color=(.8,.8,.8))
+for i in np.random.choice(neg, 300, False):
+    plt.plot(range(500), X[i,500:1000], color=(1.,0.,0.,.6))
+plt.title("Random trajectories")
+plt.show()
+
+# The max values for each line
+max_v = X[:,500:1000].max(axis=1)
+min_v = X[:,500:1000].min(axis=1)
+amp_v = max_v-min_v
+var_v = X[:,500:1000].var(axis=1)
+mean_v = X[:,500:1000].mean(axis=1)
+
+# Distribution of log-amplitudes
+plt.figure()
+plt.hist(np.log(amp_v[pos]), bins=50, color=(.8,.8,.8,1.))
+plt.hist(np.log(amp_v[neg]), bins=50, color=(1.,0.,0.,.6))
+plt.title("Distribution of log-amplitudes")
+
+# Distribution of log-variances
+plt.figure()
+plt.hist(np.log(var_v[pos]), bins=50, color=(.8,.8,.8,1.))
+plt.hist(np.log(var_v[neg]), bins=50, color=(1.,0.,0.,.6))
+plt.title("Distribution of log-variances")
+
+# Distribution of means
+plt.figure()
+plt.hist(mean_v[pos], bins=50, color=(.8,.8,.8,1.))
+plt.hist(mean_v[neg], bins=50, color=(1.,0.,0.,.6))
+plt.title("Distribution of means")
