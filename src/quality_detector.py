24b2323364ed41020be9cb7d12223f12d51c23cb
ctiennot <c.tiennot@gmail.com>
2016-05-21 12:49:18 +0200

    Add quick plot of predictions on top of filtered signals

--- src/quality_detector.py
+++ src/quality_detector.py
@@ -3,14 +3,17 @@
 from easy_import import extract_signals
 import numpy as np
 import functions as fun
+import matplotlib.pyplot as plt
 
 
-def predict_quality(record):
+def predict_quality(record, filtered_signal = False):
     """Predict the quality of the signal.
 
     Input
     =====
     record: path to a record
+    filtered_signal: if true then return the input signal array as the second
+    element of a tuple
 
     Output
     ======
@@ -42,9 +45,29 @@ def predict_quality(record):
     y_pred = [np.hstack((y_pred[i],na_array)) for i in range(4)]
     
     # stack the 4 channels predictions and return them
+    if filtered_signal:
+        return np.vstack((y_pred[i] for i in range(4))), filtered
+        
     return np.vstack((y_pred[i] for i in range(4)))
 
 
 if __name__ == "__main__":
     record = "../data/record1.h5"
-    results = predict_quality(record)
+    results, filtered = predict_quality(record, filtered_signal=True)
+    
+visualize = True # plot some predictions
+
+if __name__ == "__main__" and visualize:
+    subset = np.arange(100000, 1000000,1) # time range
+    ch = range(4) # channels to plot
+    
+    f, axs = plt.subplots(len(ch),1)
+    f.tight_layout()
+    for i in ch:
+        axs[i].plot(subset, filtered[i,subset]) #signal
+        axs[i].plot(subset[np.where(results[i,subset]==1)[0]], 
+             filtered[i,subset[results[i,subset]==1]], 'r-')
+        axs[i].set_title("Channel "+str(i))
+        axs[i].axes.get_xaxis().set_visible(False)
+        axs[i].axes.get_yaxis().set_visible(False)
+        
